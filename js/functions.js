e8e8563145203119363b2f3b4c3993e32847deeb
Augusto Bennemann <gutobenn@gmail.com>
2014-07-29 16:57:57 -0300

    arrow up and down

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,9 @@
 +function ($) {
   'use strict';
   
+  
+	$( ".down-button" ).click(function() {
+	    $( ".down-button .icon-current" ).toggleClass("fa fa-angle-up fa fa-angle-down");
+	});
 
 }(window.jQuery);

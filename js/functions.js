9bbc06b4069f47cf3ae0dcd876b2820e89f6f8dd
Morvana Bonin <morvanabonin@gmail.com>
2014-08-13 00:56:37 -0300

    Fax=zendo a parte de status Aberto ou Fechado do Matehackers.

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,14 @@
 +function ($) {
   'use strict';
   
-  
 	$( ".down-button" ).click(function() {
 	    $( ".down-button .icon-current" ).toggleClass("fa fa-angle-up fa fa-angle-down");
+	});
+
+	$('.open-close').on('click', function() {
+		$('.icon-padlock').toggleClass('fa fa-unlock fa fa-lock');
+		var text = $('.icon-padlock').html(
+			text === ' Matehacker Aberto!' ? ' Matehacker Aberto!' : ' Matehackers Fechado!');
 	});
 
 }(window.jQuery);

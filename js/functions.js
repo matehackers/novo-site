b5203373eb9d4c6723f9a16a84c613fc8ba1750d
Morvana Bonin <morvanabonin@gmail.com>
2014-12-10 01:15:23 -0200

    Trabalhando na funcionalidade do menu.

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,12 @@
 +function ($) {
   'use strict';
-  
-	$( ".down-button" ).click(function() {
-	    $( ".down-button .icon-current" ).toggleClass("fa fa-angle-up fa fa-angle-down");
-	});
-
-	$('.open-close').on('click', function() {
-		$('.icon-padlock').toggleClass('fa fa-unlock fa fa-lock');
-	});
+    
+    $( document ).ready(function() {
+        
+        $('#close-menu').on( 'click', function( event ) {
+            event.preventDefault();
+            $('#wrapper').toggleClass('toggled');
+        });
+    });
 
 }(window.jQuery);
